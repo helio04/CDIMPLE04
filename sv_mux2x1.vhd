a1e36d66d7d55c2528ca232b51eac6b45a20acab
Lucy Costa <30415579+lucycosta@users.noreply.github.com>
2024-12-13 09:22:10 -0300

    Atualizar o sv_mux2x1.vhd
    correção do load

--- sv_mux2x1.vhd
+++ sv_mux2x1.vhd
@@ -14,15 +14,15 @@ end sv_mux2x1;
 architecture main of sv_mux2x1 is 
 begin
 
-  V(0) <= (ld or (C0(0) and not(S))) or (ld or (C1(0) and S));
-  V(1) <= (ld or (C0(1) and not(S))) or (ld or (C1(1) and S));
-  V(2) <= (ld or (C0(2) and not(S))) or (ld or (C1(2) and S));
-  V(3) <= (ld or (C0(3) and not(S))) or (ld or (C1(3) and S));
-  V(4) <= (ld or (C0(4) and not(S))) or (ld or (C1(4) and S));
-  V(5) <= (ld or (C0(5) and not(S))) or (ld or (C1(5) and S));
-  V(6) <= (ld or (C0(6) and not(S))) or (ld or (C1(6) and S));
-  V(7) <= (ld or (C0(7) and not(S))) or (ld or (C1(7) and S));
-  V(8) <= (ld or (C0(8) and not(S))) or (ld or (C1(8) and S));
-  V(9) <= (ld or (C0(9) and not(S))) or (ld or (C1(9) and S));
+  V(0) <= (ld and (C0(0) and not(S))) or (ld and (C1(0) and S));
+  V(1) <= (ld and (C0(1) and not(S))) or (ld and (C1(1) and S));
+  V(2) <= (ld and (C0(2) and not(S))) or (ld and (C1(2) and S));
+  V(3) <= (ld and (C0(3) and not(S))) or (ld and (C1(3) and S));
+  V(4) <= (ld and (C0(4) and not(S))) or (ld and (C1(4) and S));
+  V(5) <= (ld and (C0(5) and not(S))) or (ld and (C1(5) and S));
+  V(6) <= (ld and (C0(6) and not(S))) or (ld and (C1(6) and S));
+  V(7) <= (ld and (C0(7) and not(S))) or (ld and (C1(7) and S));
+  V(8) <= (ld and (C0(8) and not(S))) or (ld and (C1(8) and S));
+  V(9) <= (ld and (C0(9) and not(S))) or (ld and (C1(9) and S));
   
 end architecture;
