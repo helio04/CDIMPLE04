68dd31ec669ad25cd614428fe1e60aff334ed4cd
Lucy Costa <30415579+lucycosta@users.noreply.github.com>
2024-12-13 21:21:10 -0300

    Update sv_fulladder.vhd
    toc

--- sv_fulladder.vhd
+++ sv_fulladder.vhd
@@ -11,7 +11,7 @@ end entity;
 architecture main of sv_fulladder is
 begin
 	s <= (not(a) and not(b) and ci) or (not(a) and b and not(ci)) or (a and not(b) and not(ci)) or (a and b and ci) ;
-  co <= (a and b) or (ci and (a or b));
+	co <= (a and b) or (ci and (a or b));
 end architecture;
 
 -- subtrator
