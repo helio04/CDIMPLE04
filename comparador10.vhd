2fbe7eabcdaae427e259a8541341fe65425869ca
danteemiliano <78873817+danteemiliano@users.noreply.github.com>
2024-12-14 17:49:45 -0300

    Update comparador10.vhd

--- comparador10.vhd
+++ comparador10.vhd
@@ -9,10 +9,9 @@ entity Comparador10 is
 end Comparador10;
 
 architecture logica of Comparador10 is
-    signal X : std_logic_vector(9 downto 0); -- Vetor auxiliar para comparação de igualdade         
-begin 
+    signal X : std_logic_vector(9 downto 0);    
 
-    -- Comparações de igualdade bit a bit
+begin 
     X(0) <= A(0) xnor B(0);
     X(1) <= A(1) xnor B(1);
     X(2) <= A(2) xnor B(2);
@@ -24,10 +23,8 @@ begin
     X(8) <= A(8) xnor B(8);
     X(9) <= A(9) xnor B(9);
 
-    -- A igual a B
     AIgualB <= X(9) and X(8) and X(7) and X(6) and X(5) and X(4) and X(3) and X(2) and X(1) and X(0);
     
-    -- A maior que B
     AMaiorB <= (A(9) and (not B(9)))
            or (X(9) and A(8) and (not B(8)))
            or (X(9) and X(8) and A(7) and (not B(7)))
@@ -39,7 +36,6 @@ begin
            or (X(9) and X(8) and X(7) and X(6) and X(5) and X(4) and X(3) and X(2) and A(1) and (not B(1)))
            or (X(9) and X(8) and X(7) and X(6) and X(5) and X(4) and X(3) and X(2) and X(1) and A(0) and (not B(0)));
           
-    -- A menor que B
     AMenorB <= (B(9) and (not A(9)))
            or (X(9) and B(8) and (not A(8)))
            or (X(9) and X(8) and B(7) and (not A(7)))
